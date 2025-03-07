fa3089df82bfc9881f27ac9f90950936dc954a49
Angel Astorga <astorga_ms@hotmail.com>
2025-03-07 11:46:03 -0500

    adding the navigation to another local webpage, changing ip track to ip-api.com, adding styles to the content

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 //####### here we testing the default example from open-meteo API ######
 const getWeatherData = async (latitude,longitude)=>{
+    console.log(latitude)
+    console.log(longitude)
     const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude.toPrecision(4).toString()+'&longitude='+longitude.toPrecision(4).toString()+'&current=temperature_2m,wind_speed_10m&temperature_unit=celsius');
     const myJson= await response.json();
     return myJson;
@@ -9,7 +11,8 @@ const getWeatherData = async (latitude,longitude)=>{
 //####### here we testing two APIs api64 to get the IP of the client and api.ipstack to get the details of the geographic location ######
 const getLocationWeather = async ()=>{
     const ip=await fetch('https://api64.ipify.org?format=json').then(response => response.json()).then(data => data.ip);
-    const response = await fetch('http://api.ipstack.com/'+ip+'?access_key=bc25c3f5fb36d013387d644c98881681');
+    console.log(ip)
+    const response = await fetch('http://ip-api.com/json/'+ip);
     const myJson= await response.json();
     return myJson;
 }
@@ -18,15 +21,15 @@ const getLocationWeather = async ()=>{
 async function main(){
 
     locationJson=await getLocationWeather();
-    weatherJson=await getWeatherData(locationJson.latitude, locationJson.longitude); // sending the current location to get the weather
+    weatherJson=await getWeatherData(locationJson.lat, locationJson.lon); // sending the current location to get the weather
 
     //### to check in console the structure of the data
     console.log(locationJson);
     console.log(weatherJson);
 
     //### showing the results in index.html
-    document.getElementById('country').innerHTML = locationJson.country_name;
-    document.getElementById('region').innerHTML = locationJson.region_name;
+    document.getElementById('country').innerHTML = locationJson.country;
+    document.getElementById('region').innerHTML = locationJson.regionName;
     document.getElementById('temperature').innerHTML = weatherJson.current.temperature_2m + " °C";
 }
 
